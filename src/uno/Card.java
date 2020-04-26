778dc199153ef438f07e3f2a9572ee02e6bf9742
Gix <itsgix01@gmail.com>
2020-04-25 22:21:22 -0400

    Created deck constructor, added shuffle method

--- src/uno/Card.java
+++ src/uno/Card.java
@@ -1,18 +1,22 @@
 package uno;
 
 public class Card {
-	
+
 	String type;
 	int value;
-	
+
 	public Card(String type, int value) {
 		this.type = type;
 		this.value = value;
 	}
-	
+
 	public Card(String type) {
 		this.type = type;
 		this.value = -1;
+	}
+
+	public String toString() {
+		return "Type: " + this.type + ", Value:" + this.value;
 	}
 
 }
--- src/uno/Deck.java
+++ src/uno/Deck.java
@@ -1,16 +1,40 @@
 package uno;
 
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class Deck {
-	
+
 	private LinkedList<Card> deck = new LinkedList<Card>();
-	
+
+	private final String[] colors = { "Blue", "Yellow", "Green", "Red" };
+
 	public Deck() {
-		
+		for (String i : colors) {
+			for (int j = 0; j <= 12; j++) {
+				this.deck.add(new Card(i, j));
+				if (j > 0) {
+					this.deck.add(new Card(i, j));
+				}
+			}
+		}
+		for (int i = 0; i < 4; i++) {
+			this.deck.add(new Card("Wild"));
+			this.deck.add(new Card("Wild+4"));
+		}
 	}
-	
+
+	public void Shuffle() {
+		Collections.shuffle(this.deck);
+	}
+
 	public void Deal() {
-		
+
 	}
+
+	/**
+	 * public static void main(String[] args) { Deck d = new Deck(); for(Card
+	 * card:d.deck) { System.out.println(card); } d.Shuffle(); for(Card card:d.deck)
+	 * { System.out.println(card); } }
+	 */
 }
