c7561a8028546b4f3b5646ae71d3e3be60aaae10
Stefan Winkler <itsgix01@gmail.com>
2020-04-27 19:47:07 -0400

    Added comments to deck, modified resetdeck, redone toString for cards

--- src/uno/Card.java
+++ src/uno/Card.java
@@ -5,36 +5,59 @@ public class Card {
 	String type;
 	int value;
 
+	/*
+	 * generic constructor for cards
+	 */
 	public Card(String type, int value) {
 		this.type = type;
 		this.value = value;
 	}
 
+	/*
+	 * special constructor for wildcards
+	 */
 	public Card(String type) {
 		this.type = type;
 		this.value = -1;
 	}
 
+	/*
+	 * prints out the correct card name based on value
+	 */
 	public String toString() {
-		return "Type: " + this.type + ", Value: " + this.value;
+		switch (this.value) {
+		case -1:
+			return "Type: " + this.type;
+		case 10:
+			return "Type: " + this.type + ", Value: Skip";
+		case 11:
+			return "Type: " + this.type + ", Value: Reverse";
+		case 12:
+			return "Type: " + this.type + ", Value: Draw 2";
+		default:
+			return "Type: " + this.type + ", Value: " + this.value;
+		}
 	}
-	
+
+	/*
+	 * tests if two cards are equal in type and value
+	 */
 	public boolean equals(Object o) {
-		if(o instanceof Card) {
+		if (o instanceof Card) {
 			Card other = (Card) o;
-			return (other.value==this.value && other.type.equals(this.type));
-		}else {
+			return (other.value == this.value && other.type.equals(this.type));
+		} else {
 			return false;
 		}
 	}
-	
-//	public static void main(String[] args) {
-//		Card c1 = new Card("Red", 11);
-//		Card c2 = new Card("Red", 5);
-//		Card c3 = new Card("Red", 11);
-//		
-//		System.out.println(c1.equals(c2));
-//		System.out.println(c1.equals(c3));
-//	}
-
+	/*
+	public static void main(String[] args) {
+		Card c1 = new Card("Red", 11);
+		Card c2 = new Card("Red", 5);
+		Card c3 = new Card("Red", 11);
+		
+		System.out.println(c1.equals(c2));
+		System.out.println(c1.equals(c3));
+	}
+	*/
 }
--- src/uno/Deck.java
+++ src/uno/Deck.java
@@ -5,16 +5,35 @@ import java.util.LinkedList;
 
 public class Deck {
 
+	/*
+	 * Stores the unplayed cards to be drawn from
+	 */
 	private LinkedList<Card> deck = new LinkedList<Card>();
+	/*
+	 * Stores the cards that have already been played
+	 */
 	private LinkedList<Card> discard = new LinkedList<Card>();
+	/*
+	 * Keeps track of the last played card- useful for when the deck is reset
+	 */
 	private Card latestCard;
 
+	/*
+	 * used in addCards to loop over card types
+	 */
 	private final String[] colors = { "Blue", "Yellow", "Green", "Red" };
-	
+
+	/*
+	 * Constructor, adds cards to the deck
+	 */
 	public Deck() {
 		this.addCards();
 	}
-	
+
+	/*
+	 * Adds the cards to the deck: each color 0,9; duplicates of 1,9; 
+	 * 2 skip/reverse/+2 for each color; 4 wildcards, 4 wild+4
+	 */
 	private void addCards() {
 		for (String i : colors) {
 			for (int j = 0; j <= 12; j++) {
@@ -26,54 +45,77 @@ public class Deck {
 		}
 		for (int i = 0; i < 4; i++) {
 			this.deck.add(new Card("Wild"));
-			this.deck.add(new Card("Wild+4"));
+			this.deck.add(new Card("Wild Draw 4"));
 		}
 	}
 
+	/*
+	 * randomizes the card order
+	 */
 	public void shuffle() {
 		Collections.shuffle(this.deck);
 	}
-	
+
+	/*
+	 * adds a card to the discard pile after it has been played
+	 */
 	public void addToDiscard(Card c) {
 		this.discard.add(c);
 	}
-	
+
+	/*
+	 * draws a card from the deck, and calls resetDeck if the deck is empty
+	 */
 	public Card drawCard() {
-		if(this.deck.size() > 0) {
+		if (this.deck.size() > 0) {
 			return this.deck.poll();
 		} else {
 			this.resetDeck();
 			return this.deck.poll();
 		}
-		
+
 	}
-	
+
+	/*
+	 * refills the deck with cards, and empties the discard pile
+	 */
 	public void resetDeck() {
 		this.latestCard = this.discard.getLast();
 		this.discard.clear();
 		this.discard.add(this.latestCard);
 		this.deck.clear();
 		this.addCards();
+		this.deck.remove(this.latestCard);
 		this.shuffle();
 	}
-	
+
+	/*
 	public void printSize() { //for testing purposes
 		System.out.println(this.deck.size()+", "+this.discard.size());
 	}
 	
 	
-	/*
+	
 	public static void main(String[] args) {
 		Deck d = new Deck();
 		d.shuffle();
 		d.printSize();
+		
+		for (Card s:d.deck) {
+			System.out.println(s);
+		}
+		
 		Card test = d.drawCard();
+		d.addToDiscard(test);
+		Card e = d.drawCard();
+		d.addToDiscard(test);
+		Card x = d.drawCard();
 		d.addToDiscard(test);
 		d.printSize();
 		d.resetDeck();
 		d.printSize();
-		
+			
 	}
-	**/
+	*/
 	
 }
